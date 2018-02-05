d1f69eea8ca867485f8bc15ecb8e5a596a3bd893
Louis Sanchez <louis.sa@outlook.com>
2018-02-05 16:53:26 -0500

    icons, layout, cleaned up fetch logic

--- src/App.js
+++ src/App.js
@@ -1,61 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { faCircleNotch, faExclamationCircle, faSearch } from '@fortawesome/fontawesome-free-solid'
+import { faTwitch } from '@fortawesome/fontawesome-free-brands'
 import './App.css';
 
 
 const CLIENT_ID = "9uygzutqoo30vb6730vcjwr9sryai3";
 const PATH_STREAMS = "https://api.twitch.tv/kraken/streams/"
 const PATH_USERS = "https://api.twitch.tv/kraken/users/"
-
-const PARAM_SEARCH = ""
-
-
-const stream1 = {
-  "stream": {
-     "_id": 23932774784,
-     "game": "BATMAN - The Telltale Series",
-     "viewers": 7254,
-     "video_height": 720,
-     "average_fps": 60,
-     "delay": 0,
-     "created_at": "2016-12-14T22:49:56Z",
-     "is_playlist": false,
-     "preview": {
-        "small": "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-80x45.jpg",
-        "medium": "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-320x180.jpg",
-        "large": "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-640x360.jpg",
-        "template": "https://static-cdn.jtvnw.net/previews-ttv/live_user_dansgaming-{width}x{height}.jpg"
-     },
-     "channel": {
-        "mature": false,
-        "status": "Dan is Batman? - Telltale's Batman",
-        "broadcaster_language": "en",
-        "display_name": "DansGaming",
-        "game": "BATMAN - The Telltale Series",
-        "language": "en",
-        "_id": 7236692,
-        "name": "dansgaming",
-        "created_at": "2009-07-15T03:02:41Z",
-        "updated_at": "2016-12-15T01:33:58Z",
-        "partner": true,
-        "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/dansgaming-profile_image-76e4a4ab9388bc9c-300x300.png",
-        "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/dansgaming-channel_offline_image-d3551503c24c08ad-1920x1080.png",
-        "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/dansgaming-profile_banner-4c2b8ece8cd010b4-480.jpeg",
-        "profile_banner_background_color": null,
-        "url": "https://www.twitch.tv/dansgaming",
-        "views": 63906830,
-        "followers": 538598
-     }
-  }
-}
-
-const stream2 = {
-  "stream":null
-}
-
-
-
-
 
 
 class App extends Component {
@@ -64,62 +16,28 @@ class App extends Component {
 
     this.state = {
       channels: [
-        {"id" : "esl_sc2", "displayName": "Nothing", "result" : stream1, "isLoading" : true, error: null },
-        {"id" : "dan", "displayName": "Nothing", "result" : stream1, "isLoading" : true, error: null },
-        {"id" : "ls777", "displayName": "Nothing", "result" : stream1, "isLoading" : true, error: null },
-        {"id" : "wertytreuytrerty", "displayName": "Nothing", "result" : stream1, "isLoading" : true,  error: null},
+        "esl_sc2",
+        "dan", 
+        "ls777", 
+        "wertytreuytrerty"
       ],
       error: null
-
-      
     }
 
     this.getArrayPos = this.getArrayPos.bind(this)
-    this.fetchStream = this.fetchStream.bind(this)
     this.onDismiss = this.onDismiss.bind(this)
   }
 
   onDismiss(id) {
     console.log(id);
     const {channels} = this.state
-    this.setState({channels: channels.filter(item => item.id != id)})
+    this.setState({channels: channels.filter(item => item !== id)})
   }
 
   getArrayPos(id) {
-    return this.state.channels.findIndex(item => item.id == id)
+    return this.state.channels.indexOf(id)
   }
-
-  fetchStream(id) {
-    const SearchPaths = [PATH_STREAMS, PATH_USERS]
-    Promise.all(
-      SearchPaths.map(path => 
-        fetch(`${path}${id}`,{headers:{'Client-ID':CLIENT_ID}})
-        .then(response => response.json())
-      )
-    )
-      .then(results => this.setStreamStatus(results, id))
-      .catch((e) => {
-        const {channels} = this.state
-        const pos = this.getArrayPos(id)
-        this.setState({error: e})
-      });
-      
-
-  }
-
-  setStreamStatus(results, id) {
-    const {channels} = this.state
-    const pos = this.getArrayPos(id)
-    channels[pos].results = {...results[0], ...results[1]};
-    channels[pos].isLoading = false;
-    this.setState({channels})
-
-    console.log(channels[pos].results)
-  }
-
-  componentDidMount() {
-    this.state.channels.forEach(item => this.fetchStream(item.id))
-	}
+  
 
   render() {
     const {
@@ -140,7 +58,9 @@ class App extends Component {
 
 const Header = () =>
   <div className="header">
-    <i className="fa fa-twitch fa-3x" aria-hidden="true"></i>
+    <div className="header-icon">
+      <FontAwesomeIcon icon={faTwitch} className="fa-5x"/>
+    </div>
     <h1>Twitch Streams</h1>
     <form>
       <div>Hide offline streams
@@ -149,7 +69,7 @@ const Header = () =>
       <div>
         <input id="textbox" placeholder="Add new streamer"></input>
         <button type="submit">
-        <i className="fa fa-search-plus" aria-hidden="true"></i>
+          <FontAwesomeIcon icon={faSearch}/>
         </button>
       </div>
     </form>
@@ -161,33 +81,90 @@ const Table = ({channels, onDismiss}) =>
   <ul>
     {channels.map((item) => 
       <Channel 
-        key={item.id}
-        id={item.id}
-        result={item.result} 
-        onDismiss={onDismiss}
-        isLoading={item.isLoading}/>
+        key={item}
+        id={item}
+        onDismiss={onDismiss}>
+      </Channel>
     )}
   </ul>
 
 
-const Channel = ({id, result, onDismiss, isLoading}) => {
-  if (!result.stream)
-  return (
-    <li>
-      Error user not found
-    </li>
-  )
+class Channel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "displayName": props.id, 
+      "result" : null, 
+      "onDismiss" : props.onDismiss,
+      "isLoading" : true,  
+      error: null,
+    }
 
+    this.fetchStream = this.fetchStream.bind(this)
+    this.setStreamStatus = this.setStreamStatus.bind(this)
 
-  const displayName = (result.stream.channel.display_name) || "";
+  }
+  
+  fetchStream() {
+    const SearchPaths = [PATH_STREAMS, PATH_USERS]
+    Promise.all(
+      SearchPaths.map(path =>
+        fetch(`${path}${this.props.id}`,{headers:{'Client-ID':CLIENT_ID}})
+        .then(response => response.json())
+      )
+    )
+      .then(results => this.setStreamStatus(results))
+      .catch(e => this.setState({error: e}));
+  }
 
-  return (
-    <li>
-      {displayName} - HI WHASSUP {isLoading.toString()}
-      <button onClick={() => onDismiss(id)}>close</button>
-    </li>
-  )
+  setStreamStatus(results) {
+
+    
+    const displayName = (results[1].display_name) || this.props.id;
+    this.setState({
+      result : {...results[0], ...results[1]},
+      displayName,
+      isLoading : false,
+      error: results[1].error
+    })
+    console.log({...results[0], ...results[1]});
+  }
+
+  componentDidMount() {
+    this.fetchStream();
+	}
+
+  render() {
+    const {displayName, result, onDismiss, isLoading, error} = this.state;
+
+   
+    
+    return (
+      <li>
+        <ProfPic result={result}/>
+        <div className="list-content">{displayName} - HI WHASSUP {isLoading.toString()} {error}
+
+        </div>
+        <button onClick={() => onDismiss(this.props.id)}>close</button>
+      </li>
+    )
+  }
 }
+
+const ProfPic = ({result, isLoading}) => 
+  result ? 
+    result.logo ?
+      <img src={result.logo.replace("300x300", "150x150")} className="prof-pic"></img>
+      : <div className="errorIcon prof-pic">
+          <FontAwesomeIcon icon={faExclamationCircle} className="fa-3x"/>
+        </div>
+    : <div className="loadingIcon prof-pic">
+        <FontAwesomeIcon icon={faCircleNotch} className="fa-3x fa-spin"/>
+      </div>
+
+  
+
+
 
 const Stream = (channel) =>
   <iframe
